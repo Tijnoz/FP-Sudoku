bf7e431d93f88f3310e3c0ad92f7fa3b852307e9
Martijn Hoogesteger <tijnoz@gmail.com>
2011-10-06 18:04:38 +0200

    * Initial Commit

---
+++ Structure.hs
@@ -0,0 +1,15 @@
+module Structure where
+
+import FPPrac
+
+data Board = Board [Field]
+    deriving (Show)
+
+--                 Col    Row    Sector  Options  Definite
+data Field = Field Number Number Number [Char] Char  
+instance Show Field where
+  show (Field col row sec os def) = show def
+  
+  
+allOptions = ['1','2','3','4','5','6','7','8','9'] 
+emptyBoard = Board [ Field col row ((col/3)+(row/3)*3) allOptions '8' | col <- [0..8], row <- [0..8]]
---
+++ sudoku.hs
@@ -0,0 +1,59 @@
+module Sudoku where
+
+import FPPrac hiding (replicate)
+import FPPrac.Events
+import FPPrac.Graphics
+import Data.List
+import Debug.Trace
+import Structure
+import Graphics
+
+printBoard :: Board -> IO ()
+printBoard (Board fs) = putStrLn 
+                        . unlines 
+                        $ [ unwords $ [ " "++[def] | f@(Field col row sec os def)<-fs, col==c ] | c <- [0..8] ]
+hLine = replicate 9 '-'
+
+setSquare (Board fs) col row new = Board (map set fs)
+    where
+        set f@(Field c r sec os def) 
+            | c == col && r == row
+            = (Field col row sec [] new)
+            | c == col
+            = (Field c r sec (delete new os) def)
+            | r == row
+            = (Field c r sec (delete new os) def)
+            | sec == (secCalc col row)
+            = (Field c r sec (delete new os) def)
+            | otherwise
+            = f
+        
+validMove b col row new = 
+            col > 0 && col < 9 &&
+            row > 0 && row < 9 &&
+            new `elem` allOptions &&
+            not  (new `elem` (getRow b row)) &&
+            not  (new `elem` (getCol b col)) &&
+            not  (new `elem` (getSec b (secCalc col row))) 
+
+validSet b col row new = if validMove b col row new 
+                         then setSquare b col row new
+                         else b
+            
+getRow (Board fs) row = [ d | f@(Field c r s o d)<-fs, r==row ]
+getCol (Board fs) col = [ d | f@(Field c r s o d)<-fs, c==col ]
+getSec (Board fs) sec = [ d | f@(Field c r s o d)<-fs, s==sec ]
+
+secCalc col row = (col `div` 3)+(row `div` 3)*3
+
+
+ 
+---meuk
+
+main = doShow sudokuHandler
+
+-- Event handler
+sudokuHandler :: Store -> Input -> (Store,[Output])
+
+--- Unhandled event handler
+sudokuHandler store _ = (store,[])
