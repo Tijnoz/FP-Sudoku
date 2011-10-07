e4f1f6ca2f44e8df7db726737261fc298e0306b5
Ralph Broenink <ralph@ralphbroenink.net>
2011-10-07 14:11:52 +0200

    Implementeer nieuwe manier van wijzigen van waardes

--- Graphics.hs
+++ Graphics.hs
@@ -8,11 +8,15 @@ import Structure
 import Data.List
 import Debug.Trace
 
+data Process = EnteringValue | DoingNothing deriving (Eq, Show)
+
 data Store = Store
   { board             :: Board
   , name              :: String
   , additionalHandler :: Store -> Input -> (Store,[Output])
   , clickedField      :: Maybe Field
+  , process           :: Process
+  , errorMsg          :: String
   }
 
 boardTop = 250
@@ -24,10 +28,15 @@ initStore ah = Store { board = emptyBoard
                      , name = ""
                      , additionalHandler = ah
                      , clickedField = Nothing
+                     , process = DoingNothing
+                     , errorMsg = ""
                      }
   
 -----------------------------------------------------------
--- Draws one field
+redraw :: Store -> Picture
+redraw store = Pictures $ [drawBoard (board store), drawBottomLine store]
+
+-- Draws the entire board
 drawBoard :: Board -> Picture
 drawBoard (Board fields)
     = Pictures $
@@ -35,6 +44,7 @@ drawBoard (Board fields)
       : drawLines
       : (map drawField fields)
 
+-- Draws all outer lines
 drawLines :: Picture
 drawLines
     = Pictures $
@@ -42,7 +52,8 @@ drawLines
       ++ [ Line [(boardLeft, squareTop y), (boardLeft+9*50, squareTop y)] | y <- [0..9] ]
       ++ [ Line [(squareLeft (x*3) - 1, boardTop), (squareLeft (x*3) - 1, boardTop-9*50)] | x <- [1..2] ]
       ++ [ Line [(boardLeft, squareTop (y*3) + 1), (boardLeft+9*50, squareTop (y*3) + 1)] | y <- [1..2] ]
-      
+
+-- Draws the contents of one field
 drawField :: Field -> Picture
 drawField f@(Field x y s os v)
     = Pictures
@@ -52,13 +63,36 @@ drawField f@(Field x y s os v)
     where
         x' = fromIntegral $ squareLeft x
         y' = fromIntegral $ squareTop y
+
+-- Draws the bottom line
+bottomLineHeight = 25
+bottomTextHeight = 10
         
 drawBottomLine :: Store -> Picture
 drawBottomLine store 
-    = Pictures []
-
+    = Pictures 
+      [ Translate 0 (-300 + bottomLineHeight / 2) $ Color white $ rectangleSolid 800 bottomLineHeight
+      , Color black $ Line [(-400,height1),(400,height1)]
+      , Color black $ Line [(-240,height1),(-240,-300)]
+      , Color black $ Line [(100,height1),(100,-300)]
+      , Translate (-392) height2 $ Color red   $ Scale 0.11 0.11 $ Text $ (name store)
+      , Translate (-235) height2 $ Color black $ Scale 0.11 0.11 $ Text "[n]ew; [s]ave; [S]ave as; [l]oad"
+      , if (process store) == DoingNothing && not (null (errorMsg store))
+           then Translate 120 height2 $ Color red   $ Scale 0.11 0.11 $ Text (errorMsg store)
+           else Translate 120 height2 $ Color black $ Scale 0.11 0.11 $ Text actionText
+      ]
+    where
+        height1 = -300 + bottomLineHeight
+        height2 = -300 + bottomTextHeight
+        
+        Just (Field x y s os v) = (clickedField store)
+        actionText = (case (process store) of
+                      DoingNothing -> "Click field to edit";
+                      EnteringValue -> "Enter new value for " ++ (show y) ++ "," ++ (show x)
+                     )
 -----------------------------------------------------------
 
+-- Reads a board from a string
 readBoard :: String -> Board
 readBoard vs = Board (readBoardR vs 0 0)
 
@@ -68,7 +102,7 @@ readBoardR ('\n':vs) _ r = readBoardR vs 0 (r+1)  -- \n is newline => new row
 readBoardR ('\r':vs) c r = readBoardR vs c r      -- ignore \r, to be sure
 readBoardR (v:vs)    c r = (Field c r (secCalc c r) allOptions v) : (readBoardR vs (c+1) r)
 
-
+-- Normalizes a board to a string
 writeBoard :: Board -> String
 writeBoard (Board fs) = unlines $ [
                             concat $ [
@@ -82,6 +116,7 @@ writeBoard (Board fs) = unlines $ [
 
 -----------------------------------------------------------
 
+-- Returns true when the given coordinates are within the field
 inField :: Field -> (Float, Float) -> Bool
 inField f@(Field fx fy s os v) (x,y) = xmin <= x && x < xmax && ymin <= y && y < ymax
     where
@@ -90,6 +125,7 @@ inField f@(Field fx fy s os v) (x,y) = xmin <= x && x < xmax && ymin <= y && y <
         ymax = fromIntegral $ squareTop fy
         ymin = ymax - 50
 
+-- Returns either Just Field when the given coordinates are within that field, or Nothing
 onField :: [Field] -> (Float, Float) -> Maybe Field
 onField [] p = Nothing
 onField (f:fs) p
@@ -102,18 +138,18 @@ handleEvent :: Store -> Input -> (Store, [Output])
 
 --- Create new Sudoku board
 handleEvent store (KeyIn 'n')
-    = (store', [DrawPicture $ Pictures [drawBoard board', drawBottomLine store]])
+    = (store', [DrawPicture $ (redraw store')])
     where
         board' = emptyBoard
-        store' = store {board=board'}
+        store' = store {board=board', process=DoingNothing, errorMsg=""}
 
 --- Load Sudoku board
 handleEvent store (KeyIn 'l')
-    = (store,[GraphPrompt ("Load game", "filename")])
+    = (store {process=DoingNothing, errorMsg=""}, [GraphPrompt ("Load game", "filename")])
 
 handleEvent store (Prompt ("Load game", filename))
-    | filename /= "" = (store, [ReadFile filename (TXTFile "")])
-    | otherwise      = (store, [])
+    | filename /= "" = (store {process=DoingNothing, errorMsg=""}, [ReadFile filename (TXTFile "")])
+    | otherwise      = (store {process=DoingNothing, errorMsg=""}, [])
 
 --- Actual loading handled by Sudoku Handler
 --handleEvent store (File filename (TXTFile input))
@@ -125,14 +161,14 @@ handleEvent store (Prompt ("Load game", filename))
 
 --- Save Sudoku Board
 handleEvent store (KeyIn 's')
-    | name /= ""  = (store, [SaveFile name (TXTFile savetext)])
-    | otherwise   = (store, [GraphPrompt ("Save as", "filename")])
+    | name /= ""  = (store {process=DoingNothing, errorMsg=""}, [SaveFile name (TXTFile savetext)])
+    | otherwise   = (store {process=DoingNothing, errorMsg=""}, [GraphPrompt ("Save as", "filename")])
     where
         Store {board=board, name=name} = store
         savetext = writeBoard board
 
-handleEvent store (KeyIn 'a')
-    = (store, [GraphPrompt ("Save as", "filename")])
+handleEvent store (KeyIn 'S')
+    = (store {process=DoingNothing, errorMsg=""}, [GraphPrompt ("Save as", "filename")])
 
 handleEvent store (Prompt ("Save as", filename))
     = (store', [SaveFile filename (TXTFile savetext), DrawPicture $ drawBottomLine store'])
@@ -140,7 +176,7 @@ handleEvent store (Prompt ("Save as", filename))
         Store {board=board} = store
         savetext = writeBoard board
         
-        store' = store {name=filename}
+        store' = store {name=filename, process=DoingNothing, errorMsg=""}
 
 --handleEvent store (MouseUp p) = (store, [infoPanel r, infoPanelVisible])
 --    where
@@ -176,6 +212,7 @@ handleEvent store input = ah store input
 
 -----------------------------------------------------------
 
+-- Draw the screen and install the event handler
 doShow ah = installEventHandler "U Kudos (tm)" handleEvent store startPic 10
     where
         store = initStore ah
